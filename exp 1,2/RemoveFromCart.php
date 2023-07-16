bcc45d61818379558fce7826fa1065189f5b3c90
SURYA <102934593+SURYA-K123@users.noreply.github.com>
2023-07-16 21:23:18 +0530

    Add files via upload

---
+++ exp 1,2/RemoveFromCart.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "Surya@4111";
+$dbname = "web";
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['MED_ID'])) {
+    $med_id = $_POST['MED_ID'];
+
+
+    $stmt = $conn->prepare("DELETE FROM cart_items WHERE MED_ID = ?");
+    $stmt->bind_param("i", $med_id); 
+
+    if ($stmt->execute()) {
+        echo "Item deleted successfully from cart.";
+    } else {
+        echo "Error deleting item from cart: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
